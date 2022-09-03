6eab035000bed10b85250f3e40f283de43c78467
LMeister <111584068+PatZhang0214@users.noreply.github.com>
2022-09-02 21:52:25 -0400

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -2,6 +2,13 @@ public class mergeSort {
 
     public static void main(String args[]) {
         
+        /*
+            Divide an unsorted list into n sublists, each containing one element, which is considered
+            sorted. From there, merge all the sublists to new sorted sublists until there is 
+            only one sublist remaining, which will be the sorted list.
+         */ 
+        
+        
     }
     
 }
